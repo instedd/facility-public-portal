b3ee39f3ccfaa65eedc20ef300b348828a7356c1
Juan Edi <jedi@manas.com.ar>
2016-09-13 15:23:38 -0300

    CSV import script

--- app/models/elasticsearch_service.rb
+++ app/models/elasticsearch_service.rb
@@ -1,16 +1,18 @@
 class ElasticsearchService
 
+  attr_reader :client
+
   def initialize(url, index_name, should_log = Rails.env.development?)
     @client     = Elasticsearch::Client.new url: url, log: should_log
     @index_name = index_name
   end
 
   def init_index
-    @client.indices.create index: @index_name
+    client.indices.create index: @index_name
   end
 
   def init_mappings
-    @client.indices.put_mapping({
+    client.indices.put_mapping({
       index: @index_name,
       type: 'facility',
       body: {
@@ -24,8 +26,37 @@ class ElasticsearchService
     })
   end
 
+  def index_facility(facility)
+    raise "Invalid facility" unless is_valid?(facility)
+
+    client.index({
+      index: @index_name,
+      type: 'facility',
+      id: facility["id"],
+      body: {
+        name: facility["name"],
+        type: facility["type"],
+        position: {
+          lat: facility["lat"],
+          lon: facility["lng"]
+        }
+      }
+    })
+  end
+
   def self.instance
     @@instance ||= self.new(ENV['ELASTICSEARCH_URL'] || 'localhost',
                             ENV['ELASTICSEARCH_INDEX'] || 'fpp')
   end
+
+  def self.client
+    self.instance.client
+  end
+
+  private
+
+  def is_valid?(facility)
+    ["name", "type", "lat", "lng"].none? { |field| facility[field].empty? }
+  end
+
 end
