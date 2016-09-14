23078d5af30a881cdc1376fd41b9b93d79c1878c
Juan Edi <jedi@manas.com.ar>
2016-09-14 13:20:46 -0300

    Tidy up

--- app/assets/javascripts/Js.elm
+++ app/assets/javascripts/Js.elm
@@ -2,13 +2,12 @@ port module Js exposing (displayUserLocation)
 
 import Json.Encode exposing (..)
 
--- PORTS
-
 type alias Command = (String, Json.Encode.Value)
 
 port commands : Command -> Cmd msg
 
 displayUserLocation : (Float, Float) -> Cmd msg
-displayUserLocation (lat, lng) = commands ("displayUserLocation", object [ ("lat", float lat)
-                                                                         , ("lng", float lng)
-                                                                         ])
+displayUserLocation (lat, lng) = commands ("displayUserLocation"
+                                          , object [ ("lat", float lat)
+                                                   , ("lng", float lng)
+                                                   ])
