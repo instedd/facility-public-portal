6807fefbbf5cbf1117abf177a0941db0d01c17a2
Juan Edi <jedi@manas.com.ar>
2016-09-19 18:05:17 -0300

    Display search results when opening search url

--- app/assets/javascripts/Main.js.elm
+++ app/assets/javascripts/Main.js.elm
@@ -21,7 +21,6 @@ main = Navigation.programWithFlags Routing.parser
                                    , urlUpdate = Update.urlUpdate
                                    }
 
--- TODO: navigate to parsed route here
 init : Flags -> Result String Routing.Route -> (Model, Cmd Msg)
 init flags route = let model = { query = ""
                                , suggestions = Nothing
@@ -29,8 +28,8 @@ init flags route = let model = { query = ""
                                , results = Nothing
                                }
                        cmds  = [ Commands.initializeMap flags.initialPosition
-                               , if flags.fakeUserPosition
+                               , if flags.fakeUserPosition -- TODO: detect only when user presses button
                                  then Commands.fakeGeolocateUser flags.initialPosition
                                  else Commands.geolocateUser
-                               ]
+                               , Routing.navigate (Routing.routeFromResult route)]
                    in model ! cmds
