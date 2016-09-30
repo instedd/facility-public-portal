5b450a79cd141d3dda8f09d752327e5528a5f862
Juan Edi <jedi@manas.com.ar>
2016-09-30 13:49:43 -0300

    Configure fake location via environment variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def map
     @js_flags = {
       "initialPosition" => [8.979787, 38.758917],
-      "fakeUserPosition" => Rails.env.development?
+      "fakeUserPosition" => Rails.env.development? || !ENV['FAKE_USER_POSITION'].blank?
     }
   end
 end
