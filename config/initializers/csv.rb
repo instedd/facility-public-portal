c2f58752c143ce9b6113e175845500fa88bdddc1
Juan Edi <jedi@manas.com.ar>
2016-10-04 15:36:12 -0300

    Ignore SPA "blank values" when parsing CSVs
    "NULL", "Unavailable", etc.

--- config/initializers/csv.rb
+++ config/initializers/csv.rb
@@ -3,3 +3,13 @@ require "csv"
 CSV::Converters[:blank_to_nil] ||= lambda do |field|
   field && field.empty? ? nil : field
 end
+
+CSV::Converters[:spa_input_blanks] ||= lambda do |field|
+  if field && field.empty?
+    nil
+  elsif field == "NULL" || field == "Unavilable" || field == "Unavailable"
+    nil
+  else
+    field
+  end
+end
