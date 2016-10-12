712578e154903a841b766a364e6ff9754b2dffcf
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-10-12 14:26:28 -0300

    run formatter

--- app/assets/javascripts/AppFacilityDetails.elm
+++ app/assets/javascripts/AppFacilityDetails.elm
@@ -75,14 +75,14 @@ view model =
                     div
                         [ class "preloader-wrapper small active" ]
                         [ div [ class "spinner-layer spinner-blue-only" ]
-                        [
-                            div [ class "circle-clipper left" ]
-                            [ div [ class "circle" ] [] ],
-                            div [ class "gap-patch" ]
-                            [ div [ class "circle" ] [] ],
-                            div [ class "circle-clipper right" ]
-                            [ div [ class "circle" ] [] ]
-                        ] ]
+                            [ div [ class "circle-clipper left" ]
+                                [ div [ class "circle" ] [] ]
+                            , div [ class "gap-patch" ]
+                                [ div [ class "circle" ] [] ]
+                            , div [ class "circle-clipper right" ]
+                                [ div [ class "circle" ] [] ]
+                            ]
+                        ]
 
                 Loaded _ facility date _ ->
                     facilityDetail date facility
