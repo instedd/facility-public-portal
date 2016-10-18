c39e881e5d5488c8c1572639e1e441f7da82c5e3
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-10-18 16:08:03 -0300

    add docker smtp server for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,12 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Settings.smtp_settings.to_hash
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
