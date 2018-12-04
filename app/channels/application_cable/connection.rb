634200bc1533834de01ae878add30d251b64c08e
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-12-04 14:33:23 -0300

    Authenticate connections to the actioncable channels

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include ActionController::HttpAuthentication::Basic::ControllerMethods
+    include ActiveSupport::SecurityUtils
+
+    def connect
+      unless authenticate
+        reject_unauthorized_connection
+      end
+    end
+
+    private
+
+    def authenticate
+      authenticate_with_http_basic do |user, password|
+        secure_compare(user, Settings.admin_user) & secure_compare(password, Settings.admin_pass)
+      end
+    end
   end
 end
