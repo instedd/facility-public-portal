2a933ba24488813b206a9563734d81c1e0ad6236
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-12-04 14:32:54 -0300

    Datasets channel send real data

--- app/channels/datasets_channel.rb
+++ app/channels/datasets_channel.rb
@@ -1,24 +1,35 @@
 class DatasetsChannel < ActionCable::Channel::Base
   def subscribed
-    d = datasets
-    transmit(d)
+    transmit(datasets)
   end
 
   private
 
+  FILES = %w(
+    categories.csv
+    category_groups.csv
+    facilities.csv
+    facility_categories.csv
+    facility_types.csv
+    locations.csv
+  )
+
   def datasets
+    FILES.each_with_object({}) { |file, datasets|
+      datasets[file] = file_state(file)
+    }
+  end
+
+  def file_state(name)
+    path = File.join Rails.root, "data/input", name
+    return nil unless File.exists?(path)
+    stat = File.stat path
+
     {
-      "categories": {
-        "updated_at": DateTime.now,
-        "size": 234234,
-        "md5": "2b00042f7481c7b056c4b410d28f33cf",
-        "applied": false
-      },
-      "categories_groups": nil,
-      "facilities": nil,
-      "facility_categories": nil,
-      "facility_types": nil,
-      "locations": nil
+      updated_at: stat.mtime,
+      size: stat.size,
+      md5: Digest::MD5.file(path).hexdigest,
+      applied: false
     }
   end
 end
