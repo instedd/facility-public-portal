3c085b4cbb1c86a3a4cc0405899ccf47fe9dc112
Martin Verzilli <mverzilli@manas.tech>
2018-12-07 20:14:04 -0300

    Surround dataset page in a file uploader element

--- app/assets/javascripts/datasets.js
+++ app/assets/javascripts/datasets.js
@@ -8,4 +8,65 @@ $(document).ready(function() {
       app.ports.datasetEvent.send(data);
     }
   });
+
+  const fileInput = document.querySelector('#fileElem');
+  fileInput.addEventListener('change', handleFiles(fileInput.files), false);
+
+  const dropArea = document.querySelector('#drop-area');
+
+  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false);
+  });
+
+  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+  });
+
+  ['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, (e) => highlight(e), false);
+  });
+
+  ['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, (e) => unhighlight(e), false);
+  });
+
+  dropArea.addEventListener('drop', (e) => handleDrop(e), false);
+
+  function handleFiles(files) {
+    ([...files]).forEach(uploadFile);
+  }
+
+  function uploadFile(file) {
+    let url = '/datasets/upload';
+    let formData = new FormData()
+
+    formData.append('file', file)
+
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+    .then(() => { /* Done. Inform the user */ })
+    .catch(() => { /* Error. Inform the user */ })
+  }
+
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function highlight(e) {
+    dropArea.classList.add('highlight');
+  }
+
+  function unhighlight(e) {
+    dropArea.classList.remove('highlight')
+  }
+
+  function handleDrop(e) {
+    let dt = e.dataTransfer;
+    let files = dt.files;
+
+    handleFiles(files);
+  }
 });
