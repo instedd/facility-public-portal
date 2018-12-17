dbd904573fc38a0c420e85cdac41ee09ddc7c291
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-12-17 14:51:32 -0300

    Added optional "public_override" directory to serve static files with more precedence that files existing in "public" directory. Moved favicons to "public".

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FPP
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.middleware.use Rack::Deflater
+    config.middleware.insert_before ActionDispatch::Static, ActionDispatch::Static, "#{Rails.root}/public_override"
 
     config.i18n.default_locale = Settings.default_locale.to_sym
     config.i18n.available_locales = Settings.locales.keys
