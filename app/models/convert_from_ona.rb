7342ada14e2eb1bcac5f72ee488bd4345b91f48d
juanboca <jblandini@manas.com.ar>
2018-12-19 17:03:23 -0300

    Fixes #159 Import ONA survey result to facilities

--- app/models/convert_from_ona.rb
+++ app/models/convert_from_ona.rb
@@ -31,29 +31,82 @@ class ConvertFromOna
         "laboratory",
         line["section1/section1.1/Laboratory_affiliation"],
         line["section1/section1.1/laboratory_adress"],
-        line["section1/section1.1/contact_name"]
+        line["section1/section1.1/contact_name"],
+        line["section1/section1.1/contact_email"],
+        line["section1/section1.1/contact_phone"],
+        get_opening_hours(line, :en),
+        get_opening_hours(line, :fr),
+        nil,
+        line["end_time"]
       ]
     end
-
-    # id = _id
-    # name = section1/section1.1/facility_name
-    # lat = section1/section1.1/_general_gps_coordinates_latitude
-    # lng = section1/section1.1/_general_gps_coordinates_longitude
-    # location_id = section1/section1.1/city
-    # facility_type = "laboratory"
-    # ownership = section1/section1.1/Laboratory_affiliation
-    # address = section1/section1.1/laboratory_adress
-    # contact_name = section1/section1.1/contact_name
-    # contact_email = section1/section1.1/contact_email
-    # contact_phone = section1/section1.1/contact_phone
-    # opening_hours:en =
-    # opening_hours:fr =
-    # photo = null
-    # last_updated = end_time
 
     write_csv(data)
 
     logger.info "Done!"
+  end
+
+  def en_strings
+    {
+      :mon_thu => "Monday to Thursday",
+      :mon_fri => "Monday to Friday",
+      :fri => "Friday",
+      :sat => "Saturday",
+      :sun => "Sunday",
+      :to => "to",
+      :from => "from",
+    }
+  end
+
+  def fr_strings
+    {
+      :mon_thu => "Lundi à Jeudi",
+      :mon_fri => "Lundi à Vendredi",
+      :fri => "Vendredi",
+      :sat => "Samedi",
+      :sun => "Dimanche",
+      :to => "à",
+      :from => "de",
+    }
+  end
+
+  def get_opening_hours line, lang
+
+    lang_strings = lang == :en ? en_strings : fr_strings
+
+    days = line["section1/section1.1/days"]
+    opening_hours_weekday = get_hours(line["section1/section1.1/open_hoursm"])
+    closing_hours_weekday = get_hours(line["section1/section1.1/close_hoursm"])
+
+    opening_hours_friday = get_hours(line["section1/section1.1/open_hoursf"])
+    closing_hours_friday = get_hours(line["section1/section1.1/close_hoursf"])
+
+    opens_saturday = days.downcase.include? "saturday"
+    opens_sunday = days.downcase.include? "sunday"
+
+    if opening_hours_weekday != opening_hours_friday || closing_hours_weekday != closing_hours_friday
+      # "Monday to Thursday from #{opening_hours_weekday} to #{closing_hours_weekday}, Friday from #{opening_hours_friday} to #{closing_hours_friday}"
+      result = "#{lang_strings[:mon_thu]} #{lang_strings[:from]} #{opening_hours_weekday} #{lang_strings[:to]} #{closing_hours_weekday}, #{lang_strings[:fri]} #{lang_strings[:from]} #{opening_hours_friday} #{lang_strings[:to]} #{closing_hours_friday}"
+    else
+      # Monday to Friday from #{opening_hours_weekday} to #{closing_hours_weekday}
+      result = "#{lang_strings[:mon_fri]} #{lang_strings[:from]} #{opening_hours_weekday} #{lang_strings[:to]} #{closing_hours_weekday}"
+    end
+
+    if opens_saturday
+      # Saturday from #{get_hours(line["section1/section1.1/open_hourssa"])} to #{get_hours(line["section1/section1.1/close_hourssa"])}
+      result = result + ", #{lang_strings[:sat]} #{lang_strings[:from]} #{get_hours(line["section1/section1.1/open_hourssa"])} #{lang_strings[:to]} #{get_hours(line["section1/section1.1/close_hourssa"])}"
+    end
+
+    if opens_sunday
+      # Sunday from #{get_hours(line["section1/section1.1/open_hourssu"])} to #{get_hours(line["section1/section1.1/close_hourssu"])}
+      result = result + ", #{lang_strings[:sun]} #{lang_strings[:from]} #{get_hours(line["section1/section1.1/open_hourssu"])} #{lang_strings[:to]} #{get_hours(line["section1/section1.1/close_hourssu"])}"
+    end
+
+    result
+  end
+
+  def get_hours cell
+    cell.slice(0..4)
   end
 
   def write_csv(data)
