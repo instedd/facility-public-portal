2fbd21d1a52bf1ff6bf2451a99e05dcf4405d0de
juanboca <jblandini@manas.com.ar>
2018-12-20 21:44:59 -0300

    Related to #159 Adding the ability to set in the language.yml the strings used for the opening_hours for each locale Now the facilities csv generated from the ona file will contain an opening_hours column for each language available on the settings.yml, and will perform a lookup on the corresponding locale for the strings

--- app/models/convert_from_ona.rb
+++ app/models/convert_from_ona.rb
@@ -6,13 +6,18 @@ class ConvertFromOna
     @dataset = dataset
     @input_path = input_path
     @output_path = output_path
+    @locales = Settings.locales.keys
 
     @logger = Logger.new(STDOUT)
     @logger.level = Logger::INFO
   end
 
+  def get_lang_headers
+    @locales.map { |locale| "opening_hours:#{locale}"}
+  end
+
   def csv_headers
-    ["id", "name", "lat", "lng", "location_id", "facility_type", "ownership", "address", "contact_name", "contact_email", "contact_phone", "opening_hours:en", "opening_hours:fr", "photo", "last_updated"]
+    ["id", "name", "lat", "lng", "location_id", "facility_type", "ownership", "address", "contact_name", "contact_email", "contact_phone"].concat get_lang_headers().concat ["photo", "last_updated"]
   end
 
   def convert
@@ -23,22 +28,23 @@ class ConvertFromOna
     data << csv_headers
 
     @dataset[:ona_source].each do |line|
-      data << [line["_id"],
-        line["section1/section1.1/facility_name"],
-        line["section1/section1.1/_general_gps_coordinates_latitude"],
-        line["section1/section1.1/_general_gps_coordinates_longitude"],
-        line["section1/section1.1/city"],
-        "laboratory",
-        line["section1/section1.1/Laboratory_affiliation"],
-        line["section1/section1.1/laboratory_adress"],
-        line["section1/section1.1/contact_name"],
-        line["section1/section1.1/contact_email"],
-        line["section1/section1.1/contact_phone"],
-        get_opening_hours(line, :en),
-        get_opening_hours(line, :fr),
-        nil,
-        line["end_time"]
-      ]
+      data << [
+          line["_id"],
+          line["section1/section1.1/facility_name"],
+          line["section1/section1.1/_general_gps_coordinates_latitude"],
+          line["section1/section1.1/_general_gps_coordinates_longitude"],
+          line["section1/section1.1/city"],
+          "laboratory",
+          line["section1/section1.1/Laboratory_affiliation"],
+          line["section1/section1.1/laboratory_adress"],
+          line["section1/section1.1/contact_name"],
+          line["section1/section1.1/contact_email"],
+          line["section1/section1.1/contact_phone"]
+        ].concat(get_opening_hours_array(line))
+        .concat([
+          nil,
+          line["end_time"]
+        ])
     end
 
     write_csv(data)
@@ -46,34 +52,11 @@ class ConvertFromOna
     logger.info "Done!"
   end
 
-  def en_strings
-    {
-      :mon_thu => "Monday to Thursday",
-      :mon_fri => "Monday to Friday",
-      :fri => "Friday",
-      :sat => "Saturday",
-      :sun => "Sunday",
-      :to => "to",
-      :from => "from",
-    }
-  end
-
-  def fr_strings
-    {
-      :mon_thu => "Lundi à Jeudi",
-      :mon_fri => "Lundi à Vendredi",
-      :fri => "Vendredi",
-      :sat => "Samedi",
-      :sun => "Dimanche",
-      :to => "à",
-      :from => "de",
-    }
+  def get_opening_hours_array line
+    @locales.map { |locale| get_opening_hours line, locale}
   end
 
   def get_opening_hours line, lang
-
-    lang_strings = lang == :en ? en_strings : fr_strings
-
     days = line["section1/section1.1/days"]
     opening_hours_weekday = get_hours(line["section1/section1.1/open_hoursm"])
     closing_hours_weekday = get_hours(line["section1/section1.1/close_hoursm"])
@@ -86,20 +69,20 @@ class ConvertFromOna
 
     if opening_hours_weekday != opening_hours_friday || closing_hours_weekday != closing_hours_friday
       # "Monday to Thursday from #{opening_hours_weekday} to #{closing_hours_weekday}, Friday from #{opening_hours_friday} to #{closing_hours_friday}"
-      result = "#{lang_strings[:mon_thu]} #{lang_strings[:from]} #{opening_hours_weekday} #{lang_strings[:to]} #{closing_hours_weekday}, #{lang_strings[:fri]} #{lang_strings[:from]} #{opening_hours_friday} #{lang_strings[:to]} #{closing_hours_friday}"
+      result = "#{I18n.t(:mon_thu, locale: lang)} #{I18n.t(:from, locale: lang)} #{opening_hours_weekday} #{I18n.t(:to, locale: lang)} #{closing_hours_weekday}, #{I18n.t(:fri, locale: lang)} #{I18n.t(:from, locale: lang)} #{opening_hours_friday} #{I18n.t(:to, locale: lang)} #{closing_hours_friday}"
     else
       # Monday to Friday from #{opening_hours_weekday} to #{closing_hours_weekday}
-      result = "#{lang_strings[:mon_fri]} #{lang_strings[:from]} #{opening_hours_weekday} #{lang_strings[:to]} #{closing_hours_weekday}"
+      result = "#{I18n.t(:mon_fri, locale: lang)} #{I18n.t(:from, locale: lang)} #{opening_hours_weekday} #{I18n.t(:to, locale: lang)} #{closing_hours_weekday}"
     end
 
     if opens_saturday
       # Saturday from #{get_hours(line["section1/section1.1/open_hourssa"])} to #{get_hours(line["section1/section1.1/close_hourssa"])}
-      result = result + ", #{lang_strings[:sat]} #{lang_strings[:from]} #{get_hours(line["section1/section1.1/open_hourssa"])} #{lang_strings[:to]} #{get_hours(line["section1/section1.1/close_hourssa"])}"
+      result = result + ", #{I18n.t(:sat, locale: lang)} #{I18n.t(:from, locale: lang)} #{get_hours(line["section1/section1.1/open_hourssa"])} #{I18n.t(:to, locale: lang)} #{get_hours(line["section1/section1.1/close_hourssa"])}"
     end
 
     if opens_sunday
       # Sunday from #{get_hours(line["section1/section1.1/open_hourssu"])} to #{get_hours(line["section1/section1.1/close_hourssu"])}
-      result = result + ", #{lang_strings[:sun]} #{lang_strings[:from]} #{get_hours(line["section1/section1.1/open_hourssu"])} #{lang_strings[:to]} #{get_hours(line["section1/section1.1/close_hourssu"])}"
+      result = result + ", #{I18n.t(:sun, locale: lang)} #{I18n.t(:from, locale: lang)} #{get_hours(line["section1/section1.1/open_hourssu"])} #{I18n.t(:to, locale: lang)} #{get_hours(line["section1/section1.1/close_hourssu"])}"
     end
 
     result
